7bd754c7cb84fa11861d9bfd67aaaed6239fcf7d
SP <85647800+sergprl@users.noreply.github.com>
2024-10-26 21:40:39 -0600

    Added text

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
+		System.out.println("New commit?");
+		System.out.println("New commit?");
+		System.out.println("New commit?");
+		System.out.println("BLAH BLAH BLAH");
 	}
 
 }
